00398c08580590d0fc961c9ef555735d6919f273
Jaime Rios <jaime.rios.dev@outlook.com>
2020-08-14 18:51:55 -0500

    Created starter project.

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Front End Challenge
+
+## Instructions
+
+Build this illustration using HTML & CSS so it looks as close to the design as possible.
+You should, read the data from usersArray and insert it to the DOM programmatically.
+
+![design illustration](./profile_card.jpg)
---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const usersArray = [
+  {
+    id: 2,
+    name: "Jaime Rios",
+    username: "papaponmx",
+    email: "jaime.rios.dev@outlook.com",
+    address: {
+      street: "9397 Bridge Street",
+      suite: "Suite 879",
+      city: "Auburn",
+      state: "NY",
+      zipcode: "13021",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+  },
+];
